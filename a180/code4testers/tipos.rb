52299000a08a84fe1bed8ca217ddcfbec4e2436b
Quintiliano <qpnery@paschoalotto.com.br>
2021-07-21 23:02:20 -0300

    Tipos de variáveis

---
+++ a180/code4testers/tipos.rb
@@ -0,0 +1,13 @@
+#receber os valores dinamicamente
+nome = "Quintiliano"
+idade = 26.5
+legal = true
+
+#imprimir os valores na tela
+#o método class traz o tipo da variável
+puts nome
+puts nome.class
+puts idade
+puts idade.class
+puts legal
+puts legal.class
