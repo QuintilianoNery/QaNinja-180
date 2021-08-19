41fde460d06d2cf774f1fea33bf6a99cca65b81c
Quintiliano <qpnery@paschoalotto.com.br>
2021-08-19 00:44:04 -0300

    Operadores em Ruby de comparação

--- a180/code4testers/ops.rb
+++ a180/code4testers/ops.rb
@@ -39,4 +39,64 @@ end
 #retorna falso pois 5 não é maior que 10
 #maior_que(5, 10)
 
-maior_que(10, 10)
+#retorna falso, pois 10 é igual a 10
+#maior_que(10, 10)
+
+def menor_que(v1, v2)
+  puts v1 < v2
+end
+
+#retorna true, pois 5 é menor que 10
+#menor_que(5, 10)
+
+def maior_ou_igual_que(v1, v2)
+  puts v1 >= v2
+end
+
+#retorna true, pois 10 é igual a 10
+#maior_ou_igual_que(10, 10)
+
+#retorna true, pois 15 é maior que 10
+#maior_ou_igual_que(15, 10)
+
+#retorna true, pois 15 é maior que 10
+#maior_ou_igual_que(5, 10)
+
+def menor_ou_igual_que(v1, v2)
+  puts v1 <= v2
+end
+
+#retorna true, pois 5 = 5
+# menor_ou_igual_que(5, 5)
+
+#retorna true, pois 5 é menor que 10
+# menor_ou_igual_que(5, 10)
+
+#retorna false, pois 10 não é menor que 5
+# menor_ou_igual_que(10, 5)
+
+def igual(v1, v2)
+  puts v1 == v2
+end
+
+#true, pois são iguais
+# igual(5, 5)
+
+#false, pois são diferentes
+# igual(5, 10)
+
+#false, pois são tipos diferentes, um string e outro inteiro
+# igual(10, "10")
+
+def diferente(v1, v2)
+  puts v1 != v2
+end
+
+#rtue, pois são diferentes
+# diferente(10, 8)
+
+#false, pois são iguais
+# diferente(10, 10)
+
+#true, pois são diferentes
+# diferente(10, "10")
