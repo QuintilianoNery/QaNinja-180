d62c3f100ca8570a4d60f4b9d1bdc3f64f4797c6
Quintiliano <qpnery@paschoalotto.com.br>
2021-08-19 00:07:51 -0300

    Operadores em Ruby

--- a180/code4testers/ops.rb
+++ a180/code4testers/ops.rb
@@ -1,28 +1,42 @@
-#Operadores matemáticos
+# #Operadores matemáticos
 
-#Soma
-def soma(n1, n2)
-  puts n1 + n2
+# #Soma
+# def soma(n1, n2)
+#   puts n1 + n2
+# end
+
+# #subtração
+# def subtrai(n1, n2)
+#   puts n1 - n2
+# end
+
+# #Multiplicação
+# def multiplica(n1, n2)
+#   puts n1 * n2
+# end
+
+# #Multiplicação
+# #na divisão tem o resto da divisão, para dar o valor exato
+# #o método nativo .to_f é um método que converte automaticamente para float
+# #Arredondar o valor com round(valor)
+# def divide(n1, n2)
+#   resultado = (n1.to_f / n2.to_f).round(4)
+#   puts resultado
+#   puts resultado.class
+# end
+
+# divide(10, 3)
+
+#Operadores de comparação
+
+def maior_que(v1, v2)
+  puts v1 > v2
 end
 
-#subtração
-def subtrai(n1, n2)
-  puts n1 - n2
-end
+#retorna true pq 10 é maior que 5
+#maior_que(10, 5)
 
-#Multiplicação
-def multiplica(n1, n2)
-  puts n1 * n2
-end
+#retorna falso pois 5 não é maior que 10
+#maior_que(5, 10)
 
-#Multiplicação
-#na divisão tem o resto da divisão, para dar o valor exato
-#o método nativo .to_f é um método que converte automaticamente para float
-#Arredondar o valor com round(valor)
-def divide(n1, n2)
-  resultado = (n1.to_f / n2.to_f).round(4)
-  puts resultado
-  puts resultado.class
-end
-
-divide(10, 3)
+maior_que(10, 10)
