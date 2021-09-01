7fca4af30bb8c6fe1a11db4e3456a053589e322b
Quintiliano Paquini Nery <qpnery@paschoalotto.com.br>
2021-09-01 17:24:53 -0300

    interpolação com Ruby

--- a180/code4testers/carteira.rb
+++ a180/code4testers/carteira.rb
@@ -1,13 +1,14 @@
-#utilizando a função gets do ruby
 puts "Qual o seu nome?"
 nome = gets.chomp
 puts "Informe sua idade!"
 idade = gets.chomp.to_i
 
 if (idade >= 18)
-  puts nome + ", você tem " + idade.to_s + " anos e pode tirar sua carteira de motorista!"
+  #puts nome + ", você tem " + idade.to_s + " anos e pode tirar sua carteira de motorista!"
+  #interpolação utilizando #{variável}
+  puts "#{nome}, você tem #{idade} anos e pode tirar sua carteira de motorista!"
 elsif (idade >= 7)
-  puts nome + ", você tem " + idade.to_s + " anos e portanto deve continuar andando de biscicleta!"
+  puts "#{nome}, você tem #{idade} anos e portanto deve continuar andando de biscicleta!"
 else
-  puts nome + ", você tem " + idade.to_s + " anos e deve continuar andando de motoquinha!"
+  puts "#{nome}, você tem #{idade} anos e deve continuar andando de motoquinha!"
 end
